6caa4070184a0bedb694455494df9844836e6681
mploux <mploux@student.42.fr>
2018-04-07 18:57:54 +0200

    Updated the IMonitorModule interface

--- src/modules/IMonitorModule.hpp
+++ src/modules/IMonitorModule.hpp
@@ -10,14 +10,16 @@ class IMonitorModule
 
 public:
 
-  virtual ~IMonitorModule(void);
+	virtual ~IMonitorModule(void) {};
 
-  virtual void fetch(void) = 0;
-  virtual void update(void) = 0;
+    virtual void fetch(void) = 0;
+    virtual void update(void) = 0;
 
-  virtual std::string getName(void) const = 0;
-  virtual std::map<std::string, std::queue<float> > getGraphs(void) const = 0;
-  virtual std::map<std::string, std::string> getData(void) const = 0;
+    virtual const std::string &getName(void) const = 0;
+    virtual const std::map<std::string, std::deque<float> > &getGraphs(void) const = 0;
+    virtual const float &getGraphMin(void) const = 0;
+    virtual const float &getGraphMax(void) const = 0;
+    virtual const std::map<std::string, std::string> &getData(void) const = 0;
 
 };
 
