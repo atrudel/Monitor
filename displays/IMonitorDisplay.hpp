07b93a90d55ed928385655f8d9898baef7334fb8
Amric TRUDEL <atrudel@e2r10p9.42.fr>
2018-04-07 11:10:10 +0200

    display interface

---
+++ displays/IMonitorDisplay.hpp
@@ -0,0 +1,14 @@
+#ifndef IMONITORDISPLAY_HPP
+#define IMONITORDISPLAY_HPP
+
+class IMonitorDisplay
+{
+  public:
+    virtual ~IMonitorDisplay(void);
+
+    virtual void init(void) = 0;
+    virtual void update(void) = 0;
+    virtual void render(void) const = 0;
+};
+
+#endif //  IMONITORDISPLAY_HPP
