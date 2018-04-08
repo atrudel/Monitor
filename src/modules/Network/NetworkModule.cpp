4a9dc41b03b082f34a1ce0da55f24fa5d81d6b71
Leo BOUGEROLLES <lbougero@e2r10p11.42.fr>
2018-04-08 18:55:46 +0200

    coucou

--- src/modules/Network/NetworkModule.cpp
+++ src/modules/Network/NetworkModule.cpp
@@ -1,7 +1,7 @@
 
 #include "NetworkModule.hpp"
 
-NetworkModule::NetworkModule(void) : _name(""), _graphMin(0), _graphMax(-1)
+NetworkModule::NetworkModule(void) : _name("Network"), _graphMin(0), _graphMax(-1)
     , _graphs(std::map<std::string, std::deque<float> >()), _datas(std::map<std::string, std::string>())
 {
     _time = 0;
