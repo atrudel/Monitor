0c5855c86e7e43027fd12a5857aa7d15fa162a0b
Anne-laure VACHER <avacher@e2r10p10.42.fr>
2018-04-08 18:58:29 +0200

    OS bug -- Corrected'

--- src/modules/os/OSModule.cpp
+++ src/modules/os/OSModule.cpp
@@ -1,8 +1,7 @@
 #include "OSModule.hpp"
 
-OSModule::OSModule(void) : _name(""), _graphMin(0), _graphMax(0), _graphs(std::map<std::string, std::deque<float> >()), _datas(std::map<std::string, std::string>())
+OSModule::OSModule(void) : _name("OS"), _graphMin(0), _graphMax(0), _graphs(std::map< std::string, std::deque<float> >()), _datas(std::map<std::string, std::string>())
 {
-    set_osrelease();
     this->_name += " ";
     set_osname();
     this->_name += " ";
@@ -11,22 +10,22 @@ OSModule::OSModule(void) : _name(""), _graphMin(0), _graphMax(0), _graphs(std::m
 
 OSModule::~OSModule()
 {
-    //
 }
 
 void OSModule::set_osname()
 {
 
-    const char*     strtmp ="sw_vers -productName";
+    const char *strtmp = "sw_vers -productName";
     char str[256];
 
-    FILE*           ret = popen(strtmp, "r");
+    FILE *ret = popen(strtmp, "r");
 
     if (!ret)
-        return ;
+        return;
 
-    while(!feof(ret)) {
-       fgets(str, 256, ret);
+    while (!feof(ret))
+    {
+        fgets(str, 256, ret);
     }
 
     this->_name += strtok(str, "\n");
@@ -36,17 +35,17 @@ void OSModule::set_osname()
 
 void OSModule::set_osversion()
 {
-    const char*     strtmp ="sw_vers -productVersion";
+    const char *strtmp = "sw_vers -productVersion";
     char str[256];
 
-    FILE*           ret = popen(strtmp, "r");
+    FILE *ret = popen(strtmp, "r");
 
     if (!ret)
         throw std::runtime_error("sw_vers -productVersion not found !");
 
-
-    while(!feof(ret)) {
-       fgets(str, 256, ret);
+    while (!feof(ret))
+    {
+        fgets(str, 256, ret);
     }
 
     this->_name += strtok(str, "\n");
@@ -54,51 +53,41 @@ void OSModule::set_osversion()
     pclose(ret);
 }
 
-void OSModule::set_osrelease()
+void OSModule::update(void)
 {
-    char str[256];
-    size_t size = sizeof(str);
-    int ret = sysctlbyname("kern.osrelease", str, &size, NULL, 0);
-	if (!ret)
-		throw std::runtime_error("kern.osrelease not found !");
-    this->_name += str;
 }
 
-
-  void OSModule::update(void) {
-  }
-
-const std::map<std::string, std::deque<float> > &OSModule::getGraphs(void) const
-    {
-        return this->_graphs;
-    }
+const std::map< std::string, std::deque<float> > &OSModule::getGraphs(void) const
+{
+    return this->_graphs;
+}
 
 const float &OSModule::getGraphMin(void) const
 {
-        return this->_graphMin;
+    return this->_graphMin;
 }
- const float &OSModule::getGraphMax(void) const
- {
+const float &OSModule::getGraphMax(void) const
+{
     return this->_graphMax;
- }
+}
 
 const std::map<std::string, std::string> &OSModule::getData(void) const
-  {
-      return this->_datas;
-    }
+{
+    return this->_datas;
+}
 
 OSModule::OSModule(OSModule const &src)
 {
     *this = src;
 }
 
-
 const std::string &OSModule::getName(void) const
 {
     return (this->_name);
 }
 
-OSModule &OSModule::operator=(OSModule const &rhs) {
+OSModule &OSModule::operator=(OSModule const &rhs)
+{
     this->_name = rhs.getName();
     this->_graphs = rhs.getGraphs();
     this->_datas = rhs.getData();
--- src/modules/os/OSModule.hpp
+++ src/modules/os/OSModule.hpp
@@ -8,33 +8,30 @@
 
 class OSModule : public IMonitorModule
 {
-  public:
-    OSModule(void);
-    OSModule(OSModule const &src);
-    virtual  ~OSModule(void);
+public:
+  OSModule(void);
+  OSModule(OSModule const &src);
+  virtual ~OSModule(void);
 
-    OSModule &operator=(OSModule const &rhs);
+  OSModule &operator=(OSModule const &rhs);
 
   virtual void update(void);
 
   virtual const std::string &getName(void) const;
-  virtual const std::map<std::string, std::deque<float> > &getGraphs(void) const;
+  virtual const std::map< std::string, std::deque<float> > &getGraphs(void) const;
   virtual const std::map<std::string, std::string> &getData(void) const;
   virtual const float &getGraphMin(void) const;
   virtual const float &getGraphMax(void) const;
 
-
-  private:
-
-  void set_osrelease(void);
+private:
   void set_osname(void);
   void set_osversion(void);
 
-    std::string _name;
-    float _graphMin;
-    float _graphMax;
-    std::map<std::string, std::deque<float> > _graphs;
-    std::map<std::string, std::string> _datas;
+  std::string _name;
+  float _graphMin;
+  float _graphMax;
+  std::map< std::string, std::deque<float> > _graphs;
+  std::map<std::string, std::string> _datas;
 };
 
 #endif
