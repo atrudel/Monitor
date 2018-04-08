ad2e93c8e55c5a5be8b9b36c162fcc35693f5f72
mploux <mploux@student.42.fr>
2018-04-08 17:03:07 +0200

    Redirected cout to a file

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include "Core.hpp"
+#include <fstream>
 
 int main()
 {
+	std::ofstream out("logs.txt");
+	std::streambuf *coutbuf = std::cout.rdbuf();
+	std::cout.rdbuf(out.rdbuf());
+
 	Core core;
 
 	std::cout << "Hello 42" << std::endl;
 	core.start();
 //	 core.test();
+
+	std::cout.rdbuf(coutbuf);
 }
